d08922746918180f1b3fa90e99236269d656b2de
latteS <118535002+OvertimeCoder@users.noreply.github.com>
2023-01-28 21:38:56 +0900

    Started looking for extensions in the extensions directory.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use clap::Command;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 mod aext;
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 static mut EXTENSIONS: Vec<aext::Aext> = vec![];
 
 fn main() {
-    let mut sample = PathBuf::new();
-    sample.set_file_name("cpp.toml");
-    let mut path = vec![sample];
+    // Find files
+    let ext:Vec<PathBuf> = match std::fs::read_dir(Path::new(&format!("{}/extensions",std::env::current_dir().unwrap().as_os_str().to_str().unwrap()))) {
+        Ok(result) => {
+            println!("OK");
+            let mut vec:Vec<PathBuf> = vec!();
+            for i in result {
+                let entry = i.unwrap();
+                println!("ext:{}",Path::new(&entry.file_name()).extension().unwrap().to_str().unwrap());
+                if Path::new(&entry.file_name()).extension().unwrap().to_str().unwrap() == "toml" {
+                    println!("{}",entry.path().as_os_str().to_str().unwrap());
+                    vec.push(entry.path());
+                }
+            }
+            vec
+        }
+        Err(_) => {
+            vec!()
+        }
+    };
     // Do not assign anything to EXTENSION after this
-    unsafe { EXTENSIONS = find_aexts(path); }
+    unsafe { EXTENSIONS = find_aexts(ext); }
     // EXTENSIONS are guaranteed to have a value after assignment, so unwrapping is not a problem.
 
     let matches = Command::new("aext")
